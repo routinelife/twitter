d1554375d9e0319a576411e10590dc728268bf55
h8rry <harrychenca@gmail.com>
2024-01-11 10:02:38 +0800

    Fixes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,30 +1,38 @@
 class TweetsController < ApplicationController
-    def index
-        @tweets = Tweet.all
-        render 'tweets/index' # can be omitted
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+    render 'tweets/index' # can be omitted
+  end
+
+  def create
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+
+      if @tweet.save
+        render 'tweets/create' # can be omitted
+      else
+        render json: { success: false }
+      end
+    else
+      render json: { success: false }
     end
+  end
 
-    def create
-        token = cookies.signed[:twitter_session_token]
-        session = Session.find_by(token: token)
-    
-        if session
-          user = session.user
-          @tweet = user.tweets.new(tweet_params)
-    
-          if @tweet.save
-            render 'tweets/create' # can be omitted
-          else
-            render json: { success: false }
-          end
-        else
-          render json: { success: false }
-        end
-      end
+  def destroy
+    # Add your code
+  end
 
-      private
+  def index_by_user
+    # Add your code
+  end
 
-      def tweet_params
-        params.require(:tweet).permit(:message, :user_id)
-      end
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
 end
