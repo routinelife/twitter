a9484142d5ff2f46fc3eac44be5020fcd1177800
routinelife <tae.park2019@gmail.com>
2024-01-10 23:33:50 -0500

    final commit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,10 +24,34 @@ class TweetsController < ApplicationController
 
   def destroy
     # Add your code
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+       @tweet = Tweet.find_by(id: params[:id])
+      if @tweet&.destroy
+        render json: { success: true }
+      else
+        render json: { success: false }
+      end
+    else
+      render json: { success: false }
+    end
   end
+
+
+
 
   def index_by_user
     # Add your code
+    user = User.find_by(username: params[:username])
+   if user
+      @tweets = user.tweets
+      render 'tweets/index'
+
+    end
+
   end
 
   private
@@ -35,4 +59,9 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:message, :user_id)
   end
+end
+
+def index
+  @tweets = Tweet.all.order(created_at: :desc)
+  render 'tweets/index' # can be omitted
 end
